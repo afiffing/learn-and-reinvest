c704f32c88cf6ee753a45573a8d6c6b5c5f992e2
Ubuntu <ubuntu@ip-172-31-54-61.ec2.internal>
2016-02-15 07:20:58 +0000

    list program done

--- python2.7/list.py
+++ python2.7/list.py
@@ -1,33 +1,63 @@
 #!/bin/python
 
+
 L=[]
 
 n1=input()
-dict=[]
 
+dict={}
 for a in range(0,n1):
-	s = raw_input()
-	data = s.split(" ")
-        print data
-	dict.append({'comm':data[0],'v1':data[1]})
-        print dict
-    	for a in dict:
-                if (a['comm']=="append"):
-                        L.append(a['v1'])
-                elif (a['comm']=="insert"):
-                        L.insert(a['v1'],a['v2'])
-                elif (a['comm']=="remove"):
-                        L.remove(a['v1'])
-                elif (a['comm']=="pop"):
-                        L.pop(a['v1'])
-                elif (a['comm']=="index"):
-                        L.index(a['v1'])
-                elif (a['comm']=="count"):
-                        L.count(a['v1'])
-                elif (a['comm']=="sort"):
-                        L.sort()
-                elif (a['comm']=="reverse"):
-                        L.reverse()
+    s = raw_input()
+    data = s.split(' ')   
+#    print "data",data	
+#    print "L",L
+    if (len(data) == 1):
+        if ( data[0] == 'print' ):
+            print L
+        elif (data[0]=='sort'):
+            L.sort()
+        elif (data[0]=='reverse'):
+            L.reverse()
+	elif (data[0]=='pop'):
+            L.pop()
+
+    elif (len(data) == 2):
+        if (data[0]=='append'):
+            L.append(int(data[1]))
+        elif (data[0]=='remove'):
+            L.remove(int(data[1]))
+        elif (data[0]=='index'):
+            L.index(int(data[1]))
+        elif (data[0]=='count'):
+            L.count(int(data[1]))
+
+    elif ( len(data) == 3):
+        if (data[0]=='insert'):
+            L.insert(int(data[1]),int(data[2]))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    if (len(data) == 2):
+#        dict.update({'comm':data[0],'v1':data[1]})
+#    elif ( len(data) == 3):
+#        dict.update({'comm':data[0],'v1':data[1],'v2':data[2]})
+#    else:
+#        dict.update({'comm':data[0]})  
 
