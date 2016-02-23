2156a9266aa0a90c943664a241251d1a17d50948
Ubuntu <ubuntu@ip-172-31-54-61.ec2.internal>
2016-02-23 08:19:47 +0000

    control goes to mac pro

---
+++ python2.7/minion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+s = raw_input().strip()
+S =[]
+K =[]
+for i in s : 
+	if i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
+	
--- python2.7/sherlythebeast.py
+++ python2.7/sherlythebeast.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
-
 import sys
 
+i3=0
+i5=0
 
 t = int(raw_input().strip())
 for a0 in xrange(t):
 	n = int(raw_input().strip())
-	for i in 
+	if n == 1 :
+		print '-1'
+	elif n>1  :
+ 		
+	for i in s:
+		print i
+		if i=='3' or i=='5':
+			if i=='3':
+				i3=i3+1
+			elif i=='5':
+				i5=i5+1
+	
+print i3%5,i5%3
