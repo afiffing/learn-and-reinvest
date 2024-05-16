89d3a7dae2a3c8159890bed7c4e3edd5ca43abda
Ashish Singh <ashish.singh@gojek.com>
2024-05-16 17:34:32 +0530

    fix: linklist created and swapped pairs via recursion

--- python3/Competitive programming/recursion/link-list-swap-pairs.py
+++ python3/Competitive programming/recursion/link-list-swap-pairs.py
@@ -2,7 +2,44 @@
 # link list https://www.youtube.com/watch?v=RhCGA4jlPmQ
 # Author: Ashish Singh
 
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
 class Solution:
+    def __init__(self):
+        self.current = None
+        self.linklist = []
+        self.addlist = ()
+
+    def traverseNode(self, data):
+        newnode = Node(data)  # address of new node, node.data = 10, node.next = None
+        self.addlist = self.addlist + (newnode,)
+        print(f"address of new node i.e. {newnode.data}: {newnode}")
+
+        if self.current is None:
+            self.current = newnode
+            print("newnode current is none")
+            return
+        else:
+            self.current.next = newnode  # current next is updated with new node
+            '''
+            1 --> address of 2
+            |
+            current
+
+            append in the list for visual reference.     
+
+            1 --> address of 2
+                    |
+                 current
+            '''
+            # print(f"next of {newnode.data}: {newnode.next}")
+
+            self.linklist.append(f"{self.current.data} --> {self.current.next}")
+            self.current = newnode
 
     def swapPairs(self, head):
         if not head or not head.next:
@@ -22,4 +59,9 @@ if __name__ == "__main__":
     ll = Solution()
     listNode = [1, 2, 3, 4]
     for node in listNode:
-        print(ll.swapPairs(node))
+        ll.traverseNode(node)
+
+    print(ll.addlist)
+
+    for add in ll.addlist:
+        print(ll.swapPairs(add))
