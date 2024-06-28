ecce7a6f931af94bff8347f8ac6478abbfa7a471
Ashish Singh <ashishsingh@Ashish-Singh.local>
2024-06-28 12:57:38 +0530

    fix: test commit

---
+++ helper/commit-push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if  [[ $# -eq 0 ]]; 
+then 
+    echo "please provide commit message"
+    exit 1
+else
+    git checkout memstrong
+    git add -A
+    git commit -m "$1"
+    git checkout master
+    git pull origin --rebase master
+    git checkout memstrong
+    git pull origin --rebase master
+    git push origin memstrong
+fi
