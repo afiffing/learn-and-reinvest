1c77905ee5d09f1c0e5af20d71ec088052ccd90e
Ashish Singh <ashishsingh@Ashish-Singh.local>
2024-07-24 22:31:33 +0530

    fix: bubble sort via recursion

--- python3/Competitive programming/sorting/1-bubble-sort-✔ copy.py
+++ python3/Competitive programming/sorting/1-bubble-sort-✔ copy.py
@@ -10,7 +10,7 @@ class Solution:
                 if arr[j]>arr[j+1]:
                     arr[j],arr[j+1] = arr[j+1],arr[j]
                     didSwap = 1
-            if didSwap == 0:         
+            if didSwap == 0:           #is
                 break
         return arr
 
