f69d2b9f4952c9f3305c8845e3e433c66f67c67f
Ashish Singh <ashishsingh@Ashish-Singh.local>
2024-07-26 07:49:58 +0530

    fix: value passed in a simpler fashion

--- python3/Competitive programming/sorting/3-bubble-sort-recursion-✔.py
+++ python3/Competitive programming/sorting/3-bubble-sort-recursion-✔.py
@@ -16,13 +16,9 @@ class Solution:
         self.bubbleSort(arr,n-1)      # equivalent of i n-1 to 0        
 
 
-#forget about passing the value
 if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        arr = list(map(int, input().strip().split()))
-        Solution().bubbleSort(arr, n)
-        for i in range(n):
-            print(arr[i],end=" ")
-        print()
+    arr = [8,7,6,1,0,9,2]
+    n = len(arr)
+    Solution().quickSort(arr,0,n-1)
+    for i in arr:
+        print(i,end=" ")
