ef9068260ca15d02af12cf33e6cc7f648a1269b0
Ashish Singh <ashish.singh@gojek.com>
2024-09-10 11:19:52 +0530

    fix: sorting revised with example, next up recursion

--- python3/Competitive programming/sorting/2-insertion-sort-✔.py
+++ python3/Competitive programming/sorting/2-insertion-sort-✔.py
@@ -3,17 +3,36 @@
 
 
 class Solution:
-    def insertionSort(self, alist, n):
-        for i in range(0,n):                        # i = 0 to n-1
+    def insertionSort(self, arr, n):
+        for i in range(n):                          # i = 0 to n-1
             j = i                                   # j = i 
-            while (j>0 and arr[j-1]>arr[j]):          # while and everything within j, arr[j-1]
+            while (j>0 and arr[j-1] > arr[j]):      # while and everything within j, arr[j-1]
                 arr[j],arr[j-1]=arr[j-1],arr[j]
                 j-=1
 
 
 if __name__ == "__main__":
     arr = [8,7,6,1,0,9,2]
-    n = len(arr)
-    Solution().insertionSort(arr,n)
+    Solution().insertionSort(arr,len(arr))
     for i in arr:
         print(i,end=" ")
+
+
+#################################################################################################################################        
+
+# # Please check the insertion sort example
+
+# arr = [8,7,6,1,0,9,2]
+
+# i= 0 to 6
+
+# j=0, j<0
+
+# i=1, j=1, 8>7, arr = [7,8,6,1,0,9,2]
+# j=0
+
+# i=2, j=2, arr[1]>arr[2], 8>6, arr = [7,6,8,1,0,9,2]
+    # i=2, j=1, arr[0]>arr[1], 8>6, arr = [6,7,8,1,0,9,2]
+
+# i=3, ....
+
