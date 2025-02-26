937e9ac61ffe94a14b42150ec22e3e01ae394f47
Ashish Singh <ashish.singh@gojek.com>
2025-02-26 14:44:04 +0530

    feat: new web whatsapp selenium automation

--- python3/Competitive programming/recursion/reverse-string.py
+++ python3/Competitive programming/recursion/reverse-string.py
@@ -2,19 +2,19 @@
 # Approach is Divide and Conquer
 
 
-class Solution:
-    def reverseStr(self, s):
-        def helper(left, right):
+class Solution():
+    def revString(self,s):
+
+        def helper(left,right):
             if left < right:
-                s[left],s[right] = s[right],s[left]
+                s[left],s[right] = s[right], s[left]
                 helper(left+1,right-1)
         helper(0,len(s)-1)
 
-
 if __name__ == "__main__":
-    s = [ "h","e","l", "l","o"]
-    sol = Solution()
+    s1 = Solution()
+    strList = [x for x in 'hello']
+    s1.revString(strList)
+    print(''.join(strList))
 
-    sol.reverseStr(s)
-    print(s)
 
